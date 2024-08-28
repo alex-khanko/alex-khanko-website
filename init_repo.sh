d2715737ccd99e8a337c554312cb77ef9eec85e8
Aliaksei K <aliaksei.k@demjee.tech>
2024-08-28 13:27:24 +0200

    move posts to root level

--- init_repo.sh
+++ init_repo.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-rm -rf content
 rm -rf public
+
+#####
+
+rm -rf content
 
 # Add blog content
 git clone https://github.com/alex-khanko/alex-khanko-blog content
+
+#####
+
+#!/bin/bash
+
+POSTS_DIR="content/posts"
+
+# Find all files and move them to the root 'posts' directory
+find "$POSTS_DIR" -type f -name "*" -exec mv {} "$POSTS_DIR" \;
+
+# Remove all empty directories within 'posts'
+find "$POSTS_DIR" -type d -empty -delete
